1d5977c56a9958c29055571b641289e7f2032a9a
samsu007 <itdev.nuansa@gmail.com>
2026-05-20 13:54:18 +0800

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http;
+namespace MyPosAccurate2026;
+
+public partial class App : Application
+{
+    public static string API_HOST { get; set; }
+    public static string API_LOGIN { get; set; }
+
+    [Obsolete]
+    public App()
+	{
+		InitializeComponent();
+
+        string publik = "https://resto.samdev.org/pos-accurate/";
+        API_HOST = publik + "api/";
+        
+        API_LOGIN = publik + "config/mobile-login-api.php";
+
+        UserAppTheme = AppTheme.Light;
+
+        MainPage = new NavigationPage(new Login());
+
+    }
+
+    //protected override Window CreateWindow(IActivationState? activationState)
+	//{
+	//	//return new Window(new AppShell());
+	//	return new Window(new Login());
+	//}
+}
